9ebd8c6bc8f1f77a7ecf48241323d102ce12bf6b
CarlosBrunodev <carlosbruno.dev@gmail.com>
2023-10-13 12:14:52 -0300

    creating handlers

--- router/routers.go
+++ router/routers.go
@@ -1,45 +1,17 @@
 package router
 
 import (
-	"net/http"
+	"github.com/CarlosBrunodev/go-rest-api/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message" : "Get opening" ,
-			})
-		})
-	}
-	{
-		v1.POST("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message" : "Post opening" ,
-			})
-		})
-	}
-	{
-		v1.DELETE("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message" : "Delete opening" ,
-			})
-		})
-	}
-	{
-		v1.PUT("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message" : "Put opening" ,
-			})
-		})
-	}
-	{
-		v1.GET("/openings", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"message" : "Get openings" ,
-			})
-		})
+		v1.GET("/opening", handler.ListOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ShowOpeningHandler)
 	}
 }
